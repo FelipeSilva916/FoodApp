7a821f878949eca2d230f27a3443f4bf728bcdc2
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-16 15:14:57 -0800

    Update ResultsShowScreen.js

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import yelp from "../api/yelp";
 
-const ResultsShowScreen = ({ result, navigation }) => {
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+
   const id = navigation.getParam("id");
-  console.log(id);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
   return (
     <>
       <Text>Results Show Screen</Text>
