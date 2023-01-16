4ef59d3add094f1fe350041c2280fc654d5c63ea
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-16 14:19:23 -0800

    Update ResultsDetail.js

--- src/screens/components/ResultsDetail.js
+++ src/screens/components/ResultsDetail.js
@@ -4,10 +4,23 @@ import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 const ResultsDetail = ({ result }) => {
   return (
     <View>
-      <Text>{result.name}</Text>
+      <Text style={styles.name}>{result.name}</Text>
+      <Image style={styles.image} source={{ uri: result.image_url }} />
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4
+  },
+  name: {
+    fontWeight: "bold"
+  }
+});
 export default ResultsDetail;
