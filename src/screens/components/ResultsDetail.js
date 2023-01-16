28bb673b597ca85b03becb6cfded5a36721c0da2
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-16 14:11:40 -0800

    Create ResultsDetail.js

---
+++ src/screens/components/ResultsDetail.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+
+const ResultsDetail = ({ result }) => {
+  return (
+    <View>
+      <Text>Result Detail</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default ResultsDetail;
