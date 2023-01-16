b607359b75cbc19213efd8215d0f083b91eea430
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-16 14:31:17 -0800

    css clean up

--- src/screens/components/ResultsDetail.js
+++ src/screens/components/ResultsDetail.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList, Image } from "react-native";
 
 const ResultsDetail = ({ result }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Text style={styles.name}>{result.name}</Text>
       <Image style={styles.image} source={{ uri: result.image_url }} />
       <Text>
@@ -17,10 +17,14 @@ const styles = StyleSheet.create({
   image: {
     width: 250,
     height: 120,
-    borderRadius: 4
+    borderRadius: 4,
+    marginBottom: 5
   },
   name: {
     fontWeight: "bold"
+  },
+  container: {
+    marginLeft: 15
   }
 });
 export default ResultsDetail;
