3b9560be906a5dbf7cb2276ccf480023b2a9f891
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-17 18:30:26 -0800

    finished app

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,7 +5,6 @@ import useResults from "../hooks/useResults";
 import ResultsList from "./components/ResultsList";
 
 const SearchScreen = () => {
-  //   console.log(props);
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
