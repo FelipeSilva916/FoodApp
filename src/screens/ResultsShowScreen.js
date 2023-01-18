42f5efe2c6859d5ced9ed69c3da9d429033aaad5
Felipe Silva <87789520+FelipeSilva916@users.noreply.github.com>
2023-01-17 18:24:22 -0800

    Update ResultsShowScreen.js

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -16,13 +16,29 @@ const ResultsShowScreen = ({ navigation }) => {
     getResult(id);
   }, []);
 
+  if (!result) {
+    return null;
+  }
+
   return (
     <>
-      <Text>Results Show Screen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      ></FlatList>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
 export default ResultsShowScreen;
